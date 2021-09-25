2ef6bd129c4bfe53f0d29cbeaf65ded418fb20b0
Glaucio Daniel <glauciodaniel@gmail.com>
2021-09-24 22:54:42 -0300

    Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 
-const Footer = styled.footer`
+const Wrap = styled.footer`
   height: 95px;
   width: 100vw;
   background-color: #181818;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  color: #fff;
 `;
-
+const Footer = ()=>{
+  return <Wrap>
+      <div>Capa do álbum</div>
+      <div>
+        <div>control</div>
+        <div>soundtrack</div>
+      </div>
+      <div>volume control</div>
+  </Wrap>
+}
 export default Footer;
